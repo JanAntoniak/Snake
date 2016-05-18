bf35d716f334a540e5da4bb67bdcd42eab8a2392
Lukasz Wlazly <lukwlazly@gmail.com>
2016-05-18 19:55:37 +0200

    new features

---
+++ com/snake/AntoniusSnake.java
@@ -0,0 +1,1 @@
+//tu bedzie main i uruchamianie gry
---
+++ com/snake/view/BoardPanel.java
@@ -0,0 +1,12 @@
+package com.snake;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+
+public class BoardPanel extends JPanel {
+    private final int COL_NUMBER;
+    private final int COL_NUMBER;
+    private final int TILE_SIZE;
+}
