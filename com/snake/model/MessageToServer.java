2fc761b2127e9f13261f08fe4c99eeca701b19e3
JanAntoniak <jasiekantoniak@gmail.com>
2016-05-19 23:41:21 +0200

    new messages between serv-client

---
+++ com/snake/model/MessageToServer.java
@@ -0,0 +1,10 @@
+package com.snake;
+
+public class MessageToServer {
+
+	public Direction direction;
+	public ProtocolFlag protocolFlag;
+	MessageToServer(Snake snake, int playerID, ProtocolFlag protocolFlag) {
+			
+	}
+}
