40a38e2cc357280cff65d6f95434686f80564a8f
JanAntoniak <jasiekantoniak@gmail.com>
2016-05-20 08:57:04 +0200

    new server and client class

---
+++ com/snake/model/Client.java
@@ -0,0 +1,41 @@
+package com.snake;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetSocketAddress;
+import java.nio.channels.SocketChannel;
+
+public class Client {
+
+    public Client() {
+
+    }
+    public void createClient() throws Exception
+    {
+        SocketChannel socket = null;
+        ObjectOutputStream oos = null;
+        ObjectInputStream ois=null;
+
+        socket = SocketChannel.open(new InetSocketAddress(7777));
+        oos = new ObjectOutputStream(socket.socket().getOutputStream());
+        ois = new ObjectInputStream(socket.socket().getInputStream());
+
+        oos.writeObject(new MessageToServer(ProtocolFlag.REQUEST));
+        MessageToClient msgCl = (MessageToClient) ( ois.readObject() );
+        if(msgCl.protocolFlag != ProtocolFlag.ACCEPT)
+            System.exit(1);
+        
+        while(true) {
+            
+            if(msgCl.protocolFlag == ProtocolFlag.EXIT)
+                break;
+        }
+        oos.flush();
+        oos.close();
+        socket.close();
+    }
+    public static void main(String[] args) throws Exception {
+        Client cl = new Client();
+        cl.createClient();
+    }
+}
---
+++ com/snake/model/Server.java
@@ -0,0 +1,70 @@
+package com.snake;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetSocketAddress;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+public class Server{
+
+    public Server() {
+
+    }
+
+    public void createServer()throws Exception
+    {
+        ServerSocketChannel server=null;
+        ObjectInputStream ois=null;
+        ObjectOutputStream oos = null;
+        SocketChannel socket=null;
+        ObjectInputStream ois2 =null;
+        ObjectOutputStream oos2 = null;
+        SocketChannel socket2=null;
+
+        server=ServerSocketChannel.open();
+        server.socket().bind(new InetSocketAddress(7777));
+        socket=server.accept();
+        socket2=server.accept();
+
+        System.out.println("Nowe polaczenie przychodzace");
+
+        ois=new ObjectInputStream(socket.socket().getInputStream());
+        oos = new ObjectOutputStream(socket.socket().getOutputStream());
+        ois2 =new ObjectInputStream(socket2.socket().getInputStream());
+        oos2 = new ObjectOutputStream(socket2.socket().getOutputStream());
+
+
+        try
+        {
+            MessageToServer obj = (MessageToServer) (ois.readObject());
+            MessageToServer obj2 = (MessageToServer) (ois2.readObject());
+            if(obj.protocolFlag != ProtocolFlag.REQUEST || obj2.protocolFlag != ProtocolFlag.REQUEST)
+                System.exit(1);
+            oos.writeObject(new MessageToClient(ProtocolFlag.ACCEPT));
+            oos2.writeObject(new MessageToClient(ProtocolFlag.ACCEPT));
+
+            while(true) {
+                /**
+                 * Tutaj wysyłamy komunikaty miedzy serverem i klientem
+                 */
+            }
+        }
+        catch(ClassNotFoundException e)
+        {
+            System.out.println("ClassNotFound");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Other exception");
+        }
+        System.out.print("4");
+        socket.close();
+        ois.close();
+        server.close();
+    }
+    static public void main(String[] args) throws Exception {
+        Server sv = new Server();
+        sv.createServer();
+    }
+}
