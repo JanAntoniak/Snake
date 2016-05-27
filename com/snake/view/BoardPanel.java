a52eff621488383c4e321595755246600ebacd00
JanAntoniak <jasiekantoniak@gmail.com>
2016-05-27 09:02:11 +0200

    changed color of snake :D

--- com/snake/view/BoardPanel.java
+++ com/snake/view/BoardPanel.java
@@ -4,35 +4,33 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.util.ArrayDeque;
 import javax.swing.JPanel;
 
 public class BoardPanel extends JPanel {
     
-	public static final int BOARD_WIDTH = 48;
-    public static final int BOARD_HEIGHT 30;
+	public static final int BOARD_WIDTH = 30;
+    public static final int BOARD_HEIGHT = 48;
     public static final int TILE_SIZE = 20;
     private static final Font FONT = new Font("Tahoma", Font.BOLD, 25);
-    private Field field;
-    private SnakeGame game;
+    //private Field field;
+    private ClientFrame clientFrame;
 
-    public BoardPanel(SnakeGame game) {
+    public BoardPanel(ClientFrame clientFrame) {
         super();
-        this.field = game.getField();
-        COL_COUNT = field.getWidth();
-        ROW_COUNT = field.getHeight();
-        this.game = game;
+        this.clientFrame = clientFrame;
 
-        setPreferredSize(new Dimension(COL_COUNT * TILE_SIZE, ROW_COUNT * TILE_SIZE));
+        setPreferredSize(new Dimension(BOARD_HEIGHT * TILE_SIZE, BOARD_WIDTH * TILE_SIZE));
         setBackground(new Color(60, 63, 65));
     }
 
     @Override
-    public void paintComponent(Graphics g) {
+    public synchronized void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        for(int x = 0; x < COL_COUNT; x++) {
-            for(int y = 0; y < ROW_COUNT; y++) {
-                BrickType type = field.getType(x, y);
+        for(int x = 0; x < BOARD_HEIGHT; x++) {
+            for(int y = 0; y < BOARD_WIDTH; y++) {
+                BrickType type = clientFrame.getField().getType(x, y);
                 if(type != BrickType.EMPTY) {
                     drawBrick(x * TILE_SIZE, y * TILE_SIZE, type, g);
                 }
@@ -41,14 +39,14 @@ public class BoardPanel extends JPanel {
 
         g.setColor(Color.BLACK);
         g.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
-        for(int x = 0; x < COL_COUNT; x++) {
-            for(int y = 0; y < ROW_COUNT; y++) {
+        for(int x = 0; x < BOARD_HEIGHT; x++) {
+            for(int y = 0; y < BOARD_WIDTH; y++) {
                 g.drawLine(x * TILE_SIZE, 0, x * TILE_SIZE, getHeight());
                 g.drawLine(0, y * TILE_SIZE, getWidth(), y * TILE_SIZE);
             }
         }
 
-        if(game.isGameOver() || game.isNewGame()) {
+        if(clientFrame.isGameOver() || clientFrame.isNewGame()) {
             g.setColor(Color.WHITE);
 
             int centerX = getWidth() / 2;
@@ -56,10 +54,10 @@ public class BoardPanel extends JPanel {
 
             String largeMessage = null;
             String smallMessage = null;
-            if(game.isNewGame()) {
+            if(clientFrame.isNewGame()) {
                 largeMessage = "Get ready to play!";
                 smallMessage = "Press Enter to start";
-            } else if(game.isGameOver()) {
+            } else if(clientFrame.isGameOver()) {
                 largeMessage = "Game Over!";
                 smallMessage = "Press Enter to Restart";
             }
@@ -69,7 +67,7 @@ public class BoardPanel extends JPanel {
         }
     }
 
-    private void drawBrick(int x, int y, BrickType type, Graphics g) {
+    private synchronized void drawBrick(int x, int y, BrickType type, Graphics g) {
 
         switch(type) {
 
@@ -79,16 +77,14 @@ public class BoardPanel extends JPanel {
                 break;
 
             case SNAKE1:
-                g.setColor(new Color(118, 178, 176) );
+                g.setColor(new Color(68, 178, 52) );
                 g.fillRect(x, y, TILE_SIZE, TILE_SIZE);
                 break;
 
             case SNAKE2:
-                g.setColor(new Color(112, 13, 114) );
+                g.setColor(new Color(54, 56, 114) );
                 g.fillRect(x, y, TILE_SIZE, TILE_SIZE);
                 break;
         }
     }
-
-
 }
