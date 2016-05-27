4c16cbb3e12921ce204d4e17e0b15e5468327a0f
JanAntoniak <jasiekantoniak@gmail.com>
2016-05-27 08:59:34 +0200

    new AntoniusSnake and SidePanel

--- com/snake/AntoniusSnake.java
+++ com/snake/AntoniusSnake.java
@@ -2,6 +2,12 @@
 
 class AntoniusSnake {
     public static void main(String[] args) {
-
+    		if(args[0] == "server") {
+    			Server server = new Server;
+    			server.Start();
+    		}
+    		else if(args[0] = "client"){
+    			Client client = new Client(args[1]);
+    		}
     }
 }
