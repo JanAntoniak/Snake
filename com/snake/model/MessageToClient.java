ce7f0ffbaea4db39130811355ffad32f8548d0b9
JanAntoniak <jasiekantoniak@gmail.com>
2016-05-27 23:18:41 +0200

    now MessageToClient has private variables, added methodes

--- com/snake/model/MessageToClient.java
+++ com/snake/model/MessageToClient.java
@@ -1,25 +1,70 @@
 package com.snake;
 
 import java.io.Serializable;
+import java.util.ArrayDeque;
 
 public class MessageToClient implements Serializable {
-    public ProtocolFlag protocolFlag;
-    public Snake snake1;
-    public Snake snake2;
-    public Fruit fruit;
-    boolean gameOver;
-    boolean winner;
+    
+    private Snake MySnake;
+    private Snake OppSnake;
+    private Fruit fruit;
+    private boolean gameOver;
+    private boolean newGame;
+    
+    MessageToClient() {
+        MySnake = new Snake();
+        OppSnake = new Snake();
+        fruit = new Fruit();
+    }
 
-    public MessageToClient(Snake snake1, Snake snake2, Fruit fruit, ProtocolFlag protocolFlag, boolean gameOver, boolean winner) {
+    public ProtocolFlag getProtocolFlag() {
+        return protocolFlag;
+    }
+
+    public Snake getMySnake() {
+        return MySnake;
+    }
+
+    public Snake getOppSnake() {
+        return OppSnake;
+    }
+
+    public Fruit getFruit() {
+        return fruit;
+    }
+
+    public boolean isGameOver() {
+        return gameOver;
+    }
+
+    private ProtocolFlag protocolFlag;
+
+    public void setMySnake(Snake mySnake) {
+        MySnake = mySnake;
+    }
+
+    public void setProtocolFlag(ProtocolFlag protocolFlag) {
         this.protocolFlag = protocolFlag;
-        this.snake1 = snake1;
-        this.snake2 = snake2;
+    }
+
+    public void setOppSnake(Snake oppSnake) {
+        OppSnake = oppSnake;
+    }
+
+    public void setFruit(Fruit fruit) {
         this.fruit = fruit;
-        this.gameOver = gameOver;
-        this.winner = winner;
     }
 
-    public MessageToClient(ProtocolFlag protocolFlag) {
-        this.protocolFlag = protocolFlag;
+    public void setGameOver(boolean gameOver) {
+        this.gameOver = gameOver;
     }
+
+    public boolean isNewGame() {
+        return newGame;
+    }
+
+    public void setNewGame(boolean newGame) {
+        this.newGame = newGame;
+    }
+
 }
