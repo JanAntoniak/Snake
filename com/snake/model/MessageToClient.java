89a8766707c6f7437ba864426a692a960da79c65
Lukasz Wlazly <lukwlazly@gmail.com>
2016-05-30 20:50:03 +0200

    comments in model directory

--- com/snake/model/MessageToClient.java
+++ com/snake/model/MessageToClient.java
@@ -3,6 +3,14 @@ package com.snake.model;
 import java.io.Serializable;
 import java.util.ArrayDeque;
 
+/**
+ * <h1>MessageToClient</h1>
+ * Class uses in communications protocol. Contains data sends from server to clients.
+ * Both players gets very similar messages, Snakes makes difference.
+ * @see Serializable
+ * @see com.snake.controller.Server
+ * @see com.snake.controller.Client
+ */
 public class MessageToClient implements Serializable {
 
     private Snake MySnake;
@@ -10,6 +18,7 @@ public class MessageToClient implements Serializable {
     private Fruit fruit;
     private boolean gameOver;
     private boolean newGame;
+    private ProtocolFlag protocolFlag;
 
     public MessageToClient() {
         MySnake = new Snake();
@@ -17,28 +26,50 @@ public class MessageToClient implements Serializable {
         fruit = new Fruit();
     }
 
+    /**
+     *
+     * @return actually sending protocol flag
+     * @see ProtocolFlag
+     */
     public ProtocolFlag getProtocolFlag() {
         return protocolFlag;
     }
 
+    /**
+     *
+     * @return body of player's snake
+     */
     public Snake getMySnake() {
         return MySnake;
     }
 
+    /**
+     *
+     * @return body of opponent's snake
+     */
     public Snake getOppSnake() {
         return OppSnake;
     }
 
+    /**
+     *
+     * @return actual fruit on the field
+     */
     public Fruit getFruit() {
         return fruit;
     }
 
+    /**
+     * @return boolean variable informing if game is ended
+     */
     public boolean isGameOver() {
         return gameOver;
     }
 
-    private ProtocolFlag protocolFlag;
-
+    /**
+     * Sets which snake is owned by the player
+     * @param mySnake
+     */
     public void setMySnake(Snake mySnake) {
         MySnake = mySnake;
     }
--- com/snake/model/ProtocolFlag.java
+++ com/snake/model/ProtocolFlag.java
@@ -1,5 +1,9 @@
 package com.snake.model;
 
+/**
+ * <h1>ProtocolFlag</h1>
+ * Enum type used for synchronising and establishing connection between server and clients
+ */
 public enum ProtocolFlag {
         REQUEST,
         ACCEPT,
